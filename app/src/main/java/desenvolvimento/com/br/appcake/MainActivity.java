b98a9635e7000755042971e3ae23e4ce4181e141
matheussanoli <matheussanoli@gmail.com>
2017-10-29 22:08:44 -0200

    Initial commit

---
+++ app/src/main/java/desenvolvimento/com/br/appcake/MainActivity.java
@@ -0,0 +1,13 @@
+package desenvolvimento.com.br.appcake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
